708cf10977e51c492f5b2ca49a6b8150f3fd7668
pranam <pranam@gmail.com>
2023-07-29 23:01:50 +0530

    portifolio structure complete

--- components/bodyparts.tsx/Education.tsx
+++ components/bodyparts.tsx/Education.tsx
@@ -7,20 +7,20 @@ function Education() {
      <div className='flex '>
       <h1>Bachelor of Technology (B.Tech) in Computer Science Engineering</h1>
       <p>Shri Shankaracharya Technical Campus, 2019 - 2023</p>
-      <span className='relative top-4'>
+      <span className='relative xl:ml-[60px] sm:mt-2 mt-4'>
       70/100
       </span>
      </div>
     <div className='flex'>
      <h1>12th Board</h1>
      <p>Kakatiya Public School, Green Valley, 2018 - 2019</p>
-     <span className='relative sm:ml-[100px] md:ml-[460px] lg:ml-[400px]'>63/100</span>
+     <span className='relative sm:ml-[100px] md:ml-[460px]  xl:ml-[640px] lg:ml-[400px]'>63/100</span>
     </div>
     <div className='flex'>
      <h1>10th Board</h1>
      <p>
 Shri Chaitanya Techno School, 2015 - 2016</p>
-     <span className='relative lg:ml-[460px] md:ml-[460px] sm:ml-[140px]'>86/100</span>
+     <span className='relative lg:ml-[460px] md:ml-[460px] xl:ml-[700px] sm:ml-[140px]'>86/100</span>
     </div>
   
 
